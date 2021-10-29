093edab356ecd375cb25eafe17ab1bd6bfb22ec1
surajgoraya <SurajGoraya@outlook.com>
2021-10-29 14:13:25 -0400

    fix: :bug: use `system/` specifier to make launchctrl understand that we're restarting a system process

--- FixCore.sh
+++ FixCore.sh
@@ -15,10 +15,10 @@ echo "-------"
 echo "Attempting to kill CoreAudio..."
 
 
-sudo launchctl kickstart -kp com.apple.audio.coreaudiod
+sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod
 killStatus=$?
 
-echo "\nlaunchctl returned code: " $killStatus "\n"
+# echo "\nlaunchctl returned code: " $killStatus "\n"
 
 if [ $killStatus == 1 ] || [ $killStatus == 2 ] || [ $killStatus == 3 ]; then
     echo "😰 Error killing CoreAudio, perhaps you don't have permissions to do so?"
