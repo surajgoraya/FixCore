e2e558e91204df9de086a4458a94385927ae97ed
surajgoraya <SurajGoraya@outlook.com>
2025-01-07 22:01:00 -0500

    fix: :ambulance: make compatible with macOS 14.4+
    Apple introduced a breaking change in macOS 14.4 which breaks `kickstart -kp` as a way to kill the coreaudiod service. The `kill` command is required now.

--- FixCore.sh
+++ FixCore.sh
@@ -1,11 +1,11 @@
 # -------------------------
-# FixCore (2024-09-18)
+# FixCore (2025-01-07)
 # @surajgoraya
-# 
+#
 # Kills the CoreAudio & bluetoothd service on macOS to get it try
 # and reboot itself. It usually fixes issues I have
 # with macOS seemingly forever *trying* to connect
-# to my AirPods. 
+# to my AirPods.
 #
 # Use at your own risk.
 # -------------------------
@@ -14,11 +14,14 @@ echo "FixCore"
 echo "-------"
 echo "Step One: Attempting to kill CoreAudio..."
 
-sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod
+# Required as of MacOS 14.4 - See https://developer.apple.com/documentation/macos-release-notes/macos-14_4-release-notes#Core-Audio
+sudo kill system/com.apple.audio.coreaudiod
+sudo launchctl kickstart -p system/com.apple.audio.coreaudiod
+
 echo "\n✅ Killed CoreAudio, it should reboot automatically."
 
 echo "\nStep Two: Kill Bluetooth Service (⛔️ WARNING: THIS WILL DISCONNECT ALL BLUETOOTH DEVICES)"
 
-sudo launchctl kickstart -kp system/com.apple.bluetoothd 
+sudo launchctl kickstart -kp system/com.apple.bluetoothd
 echo "\n✅ Killed bluetoothd, it should reboot automatically."
 echo "-------"
